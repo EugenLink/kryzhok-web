b77ed7f7b0b59fed97da68011533bb135ae5ee6a
EugenLink <eugenstr232@gmail.com>
2025-03-15 00:15:18 +0400

    dasdas

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,7 @@ import { Banners } from "@/components/Banners/Banners.jsx";
 import { Loader } from "@/components/Loader/Loader.jsx";
 import { MainList } from "@/components/MainList/MainList.jsx";
 import styles from "@/styles/Home.module.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
