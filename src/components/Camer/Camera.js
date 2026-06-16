70d8ce8d5dee655e4049e84731354c6a04132037
EugenLink <eugenstr232@gmail.com>
2026-06-16 20:55:09 +0400

    camera update

--- src/components/Camer/Camera.js
+++ src/components/Camer/Camera.js
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 export default function Camera() {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
-
+  const [data, setData] = useState(
+    "https://i.scdn.co/image/ab67616d0000b2738cdfe55fa7cf10ad94b05c1d",
+  );
   const takePhoto = () => {
     const video = videoRef.current;
     const canvas = canvasRef.current;
@@ -16,6 +18,7 @@ export default function Camera() {
 
     const imageData = canvas.toDataURL("image/png");
     console.log(imageData); // тут твоя картинка
+    setData(imageData);
   };
   useEffect(() => {
     async function startCamera() {
@@ -37,6 +40,7 @@ export default function Camera() {
     <div>
       <h2>Камера</h2>
       <button onClick={takePhoto}>Считать</button>
+      <img src={data} />
       <canvas ref={canvasRef} style={{ display: "none" }} />
       <video
         ref={videoRef}
