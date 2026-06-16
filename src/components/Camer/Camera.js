d1fa49c82dfa5eea0cc7978357f208433690a04e
EugenLink <eugenstr232@gmail.com>
2026-06-16 20:51:58 +0400

    camera update

--- src/components/Camer/Camera.js
+++ src/components/Camer/Camera.js
@@ -2,7 +2,21 @@ import React, { useRef, useEffect } from "react";
 
 export default function Camera() {
   const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
+  const takePhoto = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0);
+
+    const imageData = canvas.toDataURL("image/png");
+    console.log(imageData); // тут твоя картинка
+  };
   useEffect(() => {
     async function startCamera() {
       try {
@@ -22,6 +36,8 @@ export default function Camera() {
   return (
     <div>
       <h2>Камера</h2>
+      <button onClick={takePhoto}>Считать</button>
+      <canvas ref={canvasRef} style={{ display: "none" }} />
       <video
         ref={videoRef}
         autoPlay
